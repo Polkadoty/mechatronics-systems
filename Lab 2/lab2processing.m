1dec85c960c508a8ac795769bd76f12041ad54fe
andressuniaga <140927249+andressuniaga@users.noreply.github.com>
2024-02-07 14:13:54 -0600

    Create lab2_processing.m
    MATLAB

---
+++ Lab 2/lab2processing.m
@@ -0,0 +1,48 @@
+%% Room Temp
+clear all; clc;
+
+%Load in file 
+file0 = ['lab2sheets\room-Temp','.xlsx'];
+T = readmatrix(file0);
+
+N = length(T(:,2));
+
+freq = 1000; %Hz
+minute = 60; %minute in s
+time = N/(minute*freq);
+dt = (freq*minute)^-1;
+
+%data
+thermistordata = T(:,2);
+ICdata = T(:,4);
+thermocoupledata = T(:,6);
+
+%averages and SD
+avgthermistor = mean(thermistordata);
+uncthermistor = std(thermistordata);
+
+avgIC = mean(ICdata);
+uncIC = std(ICdata);
+
+avgthermocouple = mean(thermocoupledata);
+uncthermocouple = std(thermocoupledata);
+
+%plots: thermistor, IC, thermocouple
+
+subplot(3,1,1)
+plot(0:dt:time-dt,thermistordata,'r--',"LineWidth",1.25)
+title('Temperature Sensor Data @ Room Temperature')
+ylabel('Thermistor [Volt]')
+
+
+subplot(3,1,2)
+plot(0:dt:time-dt,ICdata,'k--',"LineWidth",1.25)
+ylabel('IC [Volt]')
+
+
+subplot(3,1,3)
+plot(0:dt:time-dt,thermocoupledata,'b--',"LineWidth",1.25)
+ylabel('Thermocouple [Celsius]')
+
+xlabel('Time [minutes]')
+
