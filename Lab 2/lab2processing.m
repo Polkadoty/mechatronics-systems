afef56f57c939af19e9290f881a2e4d56a7205f9
andressuniaga <140927249+andressuniaga@users.noreply.github.com>
2024-02-11 00:00:22 -0600

    Update lab2processing.m
    MATLAB Part 1 Data Processing Updated

--- Lab 2/lab2processing.m
+++ Lab 2/lab2processing.m
@@ -5,44 +5,67 @@ clear all; clc;
 file0 = ['lab2sheets\room-Temp','.xlsx'];
 M0 = readmatrix(file0);
 
+%Sample
 N = length(M0(:,2));
-
 freq = 1000; %Hz
 minute = 60; %minute in s
-time = N/(minute*freq);
+originaltime = N/(minute*freq);
+
+i=0;
+
+%find iteration of 1 minute
+while i<N
+    Q = length(M0(1:i,2));
+    if Q/(minute*freq) == 1
+        break;
+    end
+    i=i+1;
+end
+
+time1 = 1; % 1 minute of data
 dt = (freq*minute)^-1;
 
+%Resisitance
+ohm = 1000; % 1k ohm resistor
+
 %data
-thermistordata = M0(:,2);
-ICdata = M0(:,4);
-thermocoupledata = M0(:,6);
-
-%averages and SD
-avgthermistor = mean(thermistordata);
-uncthermistor = std(thermistordata);
-
-avgIC = mean(ICdata);
-uncIC = std(ICdata);
-
-avgthermocouple = mean(thermocoupledata);
-uncthermocouple = std(thermocoupledata);
+thermistordata = M0(1:i,2);
+ICdata = 100.*(M0(1:i,4) - 0.5); %IC data converted to C
+thermocoupledata = M0(1:i,6); % Thermistor convert to C ****
 
 %plots: thermistor, IC, thermocouple
 
 subplot(3,1,1)
-plot(0:dt:time-dt,thermistordata,'r--',"LineWidth",1.25)
+plot(0:dt:time1-dt,thermistordata,'r-',"LineWidth",1.25)
 title('Temperature Sensor Data @ Room Temperature')
 ylabel('Thermistor [Volt]')
 
 
 subplot(3,1,2)
-plot(0:dt:time-dt,ICdata,'k--',"LineWidth",1.25)
-ylabel('IC [Volt]')
+plot(0:dt:time1-dt,ICdata,'k-',"LineWidth",1.25)
+ylabel('IC [Celsius]')
 
 
 subplot(3,1,3)
-plot(0:dt:time-dt,thermocoupledata,'b--',"LineWidth",1.25)
+plot(0:dt:time1-dt,thermocoupledata,'b-',"LineWidth",1.25)
 ylabel('Thermocouple [Celsius]')
 
 xlabel('Time [minutes]')
 
+% Confidence Interval in Laboratory
+gamma = 0.95; %95 percent confidence
+
+Fz = 0.5*(1+gamma);
+
+nu = Q-1; %DOF
+
+p = (1-gamma)/2; %probability
+
+tstat = tinv(p,nu); %returns 
+
+% (SAMPLE MEANS) Averages and SD of mean
+avg_unc_thermistor = [mean(thermistordata) tstat*std(thermistordata)];
+
+avg_unc_IC = [mean(ICdata) tstat*std(ICdata)];
+
+avg_unc_thermocouple = [mean(thermocoupledata)  tstat*std(thermocoupledata)];
